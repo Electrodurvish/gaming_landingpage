fec5127811dbf0142c9bc5a35a33a5ccaba4d69e
Electrodurvish <rahoriadurvish@gmail.com>
2024-12-16 19:39:41 +0530

    Initial commit

---
+++ src/App.jsx
@@ -0,0 +1,14 @@
+import Hero from './components/Hero'
+import About from './components/About'
+
+const App = () => {
+  return (
+    <main className='relative w-screen min-h-screen overflow-x-hidden'>
+     <Hero/>
+     <About/>
+    </main>
+  )
+  
+}
+
+export default App
