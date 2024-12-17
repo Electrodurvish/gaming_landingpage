4a8ce3ad4a5cae473d7313220a2699edad988d46
Electrodurvish <rahoriadurvish@gmail.com>
2024-12-17 23:22:07 +0530

    Your commit message

--- src/App.jsx
+++ src/App.jsx
@@ -1,11 +1,21 @@
 import Hero from './components/Hero'
 import About from './components/About'
+import Navbar from './components/navbar'
+import Features from './components/Features'
+import FloatingImage from './components/Story'
+import Contact from './components/contact'
+import Footer from './components/Footer'
 
 const App = () => {
   return (
-    <main className='relative w-screen min-h-screen overflow-x-hidden'>
-     <Hero/>
+    <main className='relative w-screen min-h-screen overflow-x-hidden bg-blue-50'>
+      <Navbar/>
+      <Hero/>
      <About/>
+     <Features/>
+      <FloatingImage/>
+      <Contact/>
+      <Footer/>
     </main>
   )
   
